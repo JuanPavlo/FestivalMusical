b35ce4ca3cc641aecd07ad55ba7fd50654086a59
Juan Pablo <jpahumada27@outlook.com>
2022-06-01 11:51:19 -0500

    Se realiza el watch en gulp y en npm de manera que funcione de las dos formas

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 // Extraer la funcionalidad de gulp:
 // importamos las funcionalidades de gulp 
-const {src, dest} = require("gulp");
+const {src, dest, watch} = require("gulp");
 //src sirve para extraer direcciones de archivos, dest sirve para almacenar algo en una carpeta destino
 const sass = require("gulp-sass")(require("sass"));
 
 function css(done){
-    src("src/scss/app.scss")//Identificar el archivo SASS
+    src("src/scss/**/*.scss")//Identificar el archivo SASS
         .pipe(sass())//compilarlo
         .pipe(dest("build/css"));//almacenar
     done(); // es un callback que avisa a gulp cuando se llega al final de la funcion
 }
 
+function dev(done){// dev de desarrollo
+    watch("src/scss/**/*.scss", css);// se le pasa la funcion anterio y el archivo al que se le hacen los cambios
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
