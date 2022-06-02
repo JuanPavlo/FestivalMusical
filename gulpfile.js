a9ce483b3b31971a00d0982953d78123c16ac7ba
Juan Pablo <jpahumada27@outlook.com>
2022-06-01 20:38:59 -0500

    Se empieza el proyecto se realiza el header

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 // Extraer la funcionalidad de gulp:
 // importamos las funcionalidades de gulp 
+// src sirve para extraer direcciones de archivos, dest sirve para almacenar algo en una carpeta destino
 const {src, dest, watch} = require("gulp");
-//src sirve para extraer direcciones de archivos, dest sirve para almacenar algo en una carpeta destino
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done){
     src("src/scss/**/*.scss")//Identificar el archivo SASS
+        .pipe(plumber())
         .pipe(sass())//compilarlo
         .pipe(dest("build/css"));//almacenar
     done(); // es un callback que avisa a gulp cuando se llega al final de la funcion
